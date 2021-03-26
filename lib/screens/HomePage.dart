3e6ffc634d61241b3454e537ef129653fd35d4e9
zhuxiuwu <zhuxw@133.cn>
2021-03-26 18:26:05 +0800

    flutter布局的基本使用

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -19,35 +19,27 @@ class HomePageState extends State<HomePage> {
         backgroundColor: Colors.white,
         elevation: 1,
       ),
-      body: Center(
-        child:Column(
-          children: <Widget>[
-            Expanded(
-              child:Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text('主体内容1-1'), flex: 1,
-                  ),
-                  Expanded(
-                    child: Text('主体内容1-2'), flex: 1,
-                  )
-                ]
-              )
-            ),
-            Expanded(
+      body: ListView(
+        children: <Widget>[
+          Container(
+              height:180,
+              color:Colors.blue,
               child:Column(
                 children: <Widget>[
                   Expanded(
-                    child: Text('主体内容2-1'), flex: 1,
+                    child:Container(
+                      color:Colors.green,
+                      child:Text('主题1——1',style: new TextStyle(fontSize: 18.0,color:Colors.white),textAlign: TextAlign.center,),
+                      width: double.infinity
+                    ),flex:1,
                   ),
                   Expanded(
-                    child: Text('主体内容2-2'), flex: 1,
+                    child:Text('主题1——2'),flex:1,
                   )
-                ]
-              )
-            ),
-          ]
-        )
+                ],
+              ),
+            )
+        ]
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
