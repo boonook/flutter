78f9514e31c1e12c867da960a29ea16ac7dcd71d
912925 <boonook@163.com>
2021-04-02 17:14:30 +0800

    状态管理器

---
+++ lib/Store/counter.g.dart
@@ -0,0 +1,36 @@
+part of 'counter.dart';
+
+mixin _$Counter on _Counter, Store {
+  final _$valueAtom = Atom(name: '_Counter.value');
+
+  @override
+  int get value {
+    _$valueAtom.reportRead();
+    return super.value;
+  }
+
+  @override
+  set value(int value) {
+    _$valueAtom.reportWrite(value, super.value, () {
+      super.value = value;
+    });
+  }
+
+  final _$_CounterActionController = ActionController(name: '_Counter');
+
+  @override
+  void increment() {
+    final _$actionInfo =
+        _$_CounterActionController.startAction(name: '_Counter.increment');
+    try {
+      return super.increment();
+    } finally {
+      _$_CounterActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''value: ${value}''';
+  }
+}
--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import '../Store/counter.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  const LoginScreen();
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
   static const String routeName = "login";
-
-  initState() {
+  final Counter counter = Counter();
+  @override
+  void initState() {
+    super.initState();
     print('login');
+  }
 
+  LoginScreenState() {
+    print('123123');
   }
 
   @override
@@ -18,12 +31,28 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("点击跳转"),
-            RaisedButton(
+            new Text("点击跳转"),
+            new RaisedButton(
               onPressed: () {
                 Navigator.of(context).pushReplacementNamed("router/home");
               },
               child: Text("跳转"),
+            ),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  const Text('状态管理器'),
+                  Observer(
+                    builder: (_) => Text(
+                      '${counter.value}',
+                      style: const TextStyle(fontSize: 40),
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: counter.increment,
+                      child: const Text('Add Counter'))
+                ],
+              ),
             )
           ],
         ),
